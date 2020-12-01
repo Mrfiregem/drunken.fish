81d4e6e0a92fdc257d886aaea7888e215e648f40
Firegem <firinggems@gmail.com>
2020-12-01 09:23:52 -0500

    fix deprications, add more cask abbrs

--- conf.d/drunken.fish
+++ conf.d/drunken.fish
@@ -1,4 +1,5 @@
 abbr -g br brew
+abbr -g cask 'brew cask'
 
 function brewup
     brew -v update
@@ -8,19 +9,25 @@ function brewup
     brew doctor
 end
 
-abbr -g cask 'brew cask'
 abbr -g bi 'brew info'
 abbr -g bis 'brew install'
 abbr -g bus 'brew uninstall'
-abbr -g ci 'brew cask info'
-abbr -g cis 'brew cask install'
-abbr -g cus 'brew cask uninstall'
+
+abbr -g ci 'brew info --cask'
+abbr -g cis 'brew install --cask'
+abbr -g cus 'brew uninstall --cask'
+
 abbr -g bs 'brew search'
 abbr -g bsd 'brew search --desc'
+abbr -g cs 'brew search --cask'
+
 abbr -g bdr 'brew doctor'
 abbr -g bcz 'brew cask zap'
+
 abbr -g blv 'brew leaves'
-abbr -g bls 'brew ls'
+abbr -g bls 'brew list'
+abbr -g blsd 'brew list --desc'
+abbr -g cls 'brew list --cask'
 
 if brew tap | grep -q rmtree
     abbr -g brmt 'brew rmtree'
